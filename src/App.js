ba8e1a952bf4a21858a048803aee45ce911a858e
Dmitry Sky <dmitry.sky@icloud.com>
2022-12-31 16:20:37 -0700

    Display list of books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -15,7 +16,8 @@ function App() {
   };
   console.log("Books:", books)
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,16 @@
-function BookList() {
-  return <div>BookList</div>;
+function BookList({ books }) {
+  const renderedBooks = () => {
+    return books.map((book) => {
+      return <li key={book.id}>{book.title}</li>;
+    });
+  };
+
+  return (
+    <div className="book-list">
+      <h3>Books: </h3>
+      <ul>{renderedBooks()}</ul>
+    </div>
+  )
 }
 
 export default BookList;
