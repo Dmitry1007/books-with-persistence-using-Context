91a2956d07a595d0bdfa6eec08427b3efb5d746b
Dmitry Sky <dmitry.sky@icloud.com>
2022-12-31 08:42:31 -0700

    reformat files and hardcode basic create book functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React, { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div className="App">App</div>;
+  const [books, setBooks] = useState([]);
+
+  function createBook(book) {
+    setBooks([...books, book]);
+  }
+  console.log("books", books);
+  return (
+    <div className="App">
+      App
+      <BookCreate createBook={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,17 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+function BookCreate({ createBook }) {
+  
+  function handleCreate() {
+    createBook({
+      id: 1,
+      title: "The Hobbit",
+    });
+  }
+
+  return (
+    <div>BookCreate
+      <button title="Create" onClick={handleCreate}></button>
+    </div>
+  )
 }
 
 export default BookCreate;
