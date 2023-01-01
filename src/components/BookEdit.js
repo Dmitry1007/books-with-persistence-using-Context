d5dc9db0287c588646d513efa5aed0017096cf2c
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-01 14:24:17 -0700

    Enable state in BookEdit to hangle change in input field

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
   return (
     <div className="book-edit">
       <h3>Edit Book</h3>
       <form>
         <label>Title</label>
-        <input className="input" value={book.title} />
+        <input onChange={handleChange} className="input" value={title} />
         <button className="button">Update</button>
       </form>
     </div>
